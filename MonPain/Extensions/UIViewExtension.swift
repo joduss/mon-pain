21523c31f7171ae812b6f00e8e8c49a22b106f75
Jonathan Duss <jonathan.duss@iml.unibe.ch>
2020-07-30 18:48:32 +0200

    Improve UIViewExtension to load nib named after the last superclass before UIView

--- MonPain/Extensions/UIViewExtension.swift
+++ MonPain/Extensions/UIViewExtension.swift
@@ -9,13 +9,27 @@
 import UIKit
 
 extension UIView {
+    
+    
+    /// Load the nib from the latest superclass before UIView.
+    /// - Parameter targetView: the view in which the loaded view will be added and constraints are set
+    /// to make it fill completely. description
     func loadNib(targetView: UIView) {
+        
+        let uiviewMirror = String(describing: UIView.self)
+        
+        var m = Mirror(reflecting: self)
+        while(String(describing: m.superclassMirror!.subjectType) != uiviewMirror) {
+            m = m.superclassMirror!
+        }
+        
+        let fileName = String(describing: m.subjectType)
         let bundle = Bundle(for: type(of: self))
-        let nib = bundle.loadNibNamed(String(describing: type(of: self)), owner: self, options: nil)
-                    
+        let nib = bundle.loadNibNamed(fileName, owner: self, options: nil)
+        
         let nibView = nib?.first as! UIView
         nibView.translatesAutoresizingMaskIntoConstraints = false
-
+        
         targetView.addSubview(nibView)
         
         NSLayoutConstraint.activate([
