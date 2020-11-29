2ffe0d97cba97e377e3aff4dc2c46e2194c862fe
Jonathan Duss <jonathan.duss@iml.unibe.ch>
2020-11-29 21:05:31 +0100

    Add a convenient way to detect a new installed updated to detect a message to the user.

--- MonPain/UI/NewVersionControllers/NewVersionInformationCalculatorController.swift
+++ MonPain/UI/NewVersionControllers/NewVersionInformationCalculatorController.swift
@@ -10,31 +10,36 @@ import UIKit
 
 
 class NewVersionInformationCalculatorController {
-    
+
     private let viewController: UIViewController
-    
+
     private var userDefaults: UserDefaults { return UserDefaults.standard }
-    
+
     init(viewController: UIViewController) {
         self.viewController = viewController
     }
-    
+
     func displayInfoIfNeeded() {
         displayInfoV_1_2()
     }
-    
-    
+
+
     // MARK: - check version
-    
+
     private func displayInfoV_1_2(){
-        let shouldDisplay = AppConfiguration.shared.appOpenings > 1 && userDefaults.bool(forKey: "displayInfoV_1_2")
+        #if DEBUG
+            userDefaults.removeObject(forKey: "displayInfoV_1_2")
+        #endif
+
+        let shouldDisplay = AppConfiguration.shared.appOpenings > 1 && userDefaults.bool(forKey: "displayInfoV_1_2") == false
         userDefaults.setValue(true, forKey: "displayInfoV_1_2")
-        
+        userDefaults.synchronize()
+
         guard shouldDisplay else { return }
-        
+
         let alert = UIAlertController(title: "info-new-version.1.2.title".localized, message: "info-new-version.1.2.message".localized, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "general.ok".localized, style: .default))
         viewController.present(alert, animated: true, completion: nil)
     }
-    
+
 }
--- MonPain/UI/NewVersionControllers/NewVersionInformationController.swift
+++ MonPain/UI/NewVersionControllers/NewVersionInformationController.swift
@@ -1,34 +1,34 @@
+////
+////  NewVersionInformationController.swift
+////  MonPain
+////
+////  Created by Jonathan Duss on 26.10.20.
+////  Copyright © 2020 ZaJo. All rights reserved.
+////
 //
-//  NewVersionInformationController.swift
-//  MonPain
+//import UIKit
 //
-//  Created by Jonathan Duss on 26.10.20.
-//  Copyright © 2020 ZaJo. All rights reserved.
 //
-
-import UIKit
-
-
-class NewVersionInformationSecondaryController {
-    
-    private let viewController: UIViewController
-    
-    private var userDefaults: UserDefaults { return UserDefaults.standard }
-    
-    init(viewController: UIViewController) {
-        self.viewController = viewController
-    }
-    
-    func displayInfoIfNeeded() {
-        
-    }
-    
-    
-    // MARK: - check version
-    
-    private func displayInfoV_1_2() -> Bool {
-        let shouldDisplay = AppConfiguration.shared.appOpenings > 1 && userDefaults.bool(forKey: "displayInfoV_1_2")
-        user
-    }
-    
-}
+//class NewVersionInformationSecondaryController {
+//    
+//    private let viewController: UIViewController
+//    
+//    private var userDefaults: UserDefaults { return UserDefaults.standard }
+//    
+//    init(viewController: UIViewController) {
+//        self.viewController = viewController
+//    }
+//    
+//    func displayInfoIfNeeded() {
+//        
+//    }
+//    
+//    
+//    // MARK: - check version
+//    
+//    private func displayInfoV_1_2() -> Bool {
+//        let shouldDisplay = AppConfiguration.shared.appOpenings > 1 && userDefaults.bool(forKey: "displayInfoV_1_2")
+//        
+//    }
+//    
+//}
